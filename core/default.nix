3e447cee4aad241bff0b1c5678ed5bc78d721271
ngeran <ngeran@gmail.com>
2026-08-08 11:40:45 -0400

    Minor Update

--- core/default.nix
+++ core/default.nix
@@ -50,7 +50,7 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fc="
+        "nix-community.cachix.org-1:mB9FSh9qfPWdQMj6QzdK7H6YhKSpexVfOlIVS+gzrfY="
       ];
     };
     
